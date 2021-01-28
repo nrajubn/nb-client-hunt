b8d2c2663aa064e6e3b3235987dd0d991feeb6c9
nrajubn <60019513+nrajubn@users.noreply.github.com>
2021-01-27 18:34:35 -0600

    added 2 locations in init-locations.js

---
+++ init-locations.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      Name: 'Horizons west apt 105',
+      North: 40.346104,
+      West: -94.872475,
+      South: 45.9744,
+      East: 20.9100,
+      Notes: 'circle',
+      Attachments: '',
+      'Quest-Location': 'Horizons west apt 105',
+    },
+    {
+      Name: 'Bell tower',
+      North: 40.343579,
+      West: -94.89084,
+      South: 40.343268,
+      East: -94.8884,
+      Notes: 'heading south',
+      Attachments: '',
+      'Quest-Location': 'Bell tower',
+    },
+];
