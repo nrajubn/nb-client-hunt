3c9d887c7a44a73e6ff2f45eb5ba7a36af19c48d
nrajubn <60019513+nrajubn@users.noreply.github.com>
2021-01-29 22:45:23 -0600

    updated init-locations.js

--- init-locations.js
+++ init-locations.js
@@ -1,8 +1,8 @@
 export default [
     {
       Name: 'Apartment 105 Horizons west',
-         Latitude: 40.35608, 
-          Longitude: -94.88195,
+         Latitude: 40.355894, 
+          Longitude: -94.882403,
         //  North: 40.35614, 
         //  South: 40.35614, 
         //  East: -94.88189,
@@ -13,10 +13,10 @@ export default [
       Name: 'Village o aptmnts',
       // Latitude: 40.36093,
       // Longitude: -94.88797,
-      // North: 41.32622,
-      // West: -94.70588,
-      // South: 41.32622,
-      // East: -94.70588,
+      North: 41.32622,
+      West: -94.70588,
+      South: 41.32622,
+      East: -94.70588,
    
     },
 ];
